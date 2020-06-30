b91b642bcadac39e492d24c56eb8e17c7dd02e87
sonybelone <sonymekeme@gmail.com>
2020-06-30 11:44:27 +0400

    v0.1

--- fichiertpgit/jeueleve.c
+++ fichiertpgit/jeueleve.c
@@ -1,25 +1,37 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
+#include<unistd.h>
 void plus_moins(int choix_joueur,int valeur_a_trouve){
 	int x=0;
-	if(choix_joueur == valeur_a_trouve)
+	while(choix_joueur != valeur_a_trouve && x < valeur_a_trouve)
 	{
-		printf("bingo");
-	}
-	else if(choix_joueur < valeur_a_trouve)
-	{
-		printf("plus");
-	}
-	else//(choix_joueur > valeur_a_trouve)
-	{
-		printf("moins");
+		if(choix_joueur == valeur_a_trouve)
+		{
+			printf("bingo");
+		}
+		else if(choix_joueur < valeur_a_trouve)
+		{
+			printf("plus");
+		}
+		else//(choix_joueur > valeur_a_trouve)
+		{
+			printf("moins");
+		}
+		x++;
 	}
 }	
 int main()
 {
-	int x =10, y;
+	srand(time(NULL));
+	int x = rand()%100;
+	int y;
+	printf("inserer un nombre svp  \n");
+	scanf("%d",&y);
 	plus_moins(x,y);
 }
+
+
 	
 	
 
